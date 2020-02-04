8abcb3c7afaefb478d5cc00930efd8128c5754eb
Carolyn Grady <owenv8@gmail.com>
2020-02-03 22:28:50 -0500

    user search query passes to firebase and the buttons generate from the info being passed back

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,11 +10,35 @@ $("#sportsQuery-submit").on("click", function () {
   event.preventDefault();
   let sportsItem = $("#sportsQuery-text").val().trim()
   sports.push(sportsItem);
+
+  // pass to firebase
+  if (sportsItem === '') {
+    //use modular but using alert for now
+    alert("Please Enter a Player or Team Name");
+  }
+  else {
+
+    db.ref().push({ userQuery: sportsItem, });
+  }
+
   //Resets search text
   $("#sportsQuery-text").val("");
+})
+
+// passing user queries to firebase to be recalled later
+var userQuery = '';
+
+// make buttons from database
+db.ref().on('child_added', function (data) {
+
+  dv = data.val()
+
   //Creates buttons
   createButtons();
-})
+
+}), function (errorHandle) {
+  console.log("Errors occured: " + errorHandle.code)
+}
 
 //Function for creating buttons for home page
 function createButtons() {
@@ -30,7 +54,7 @@ function createButtons() {
     var sportsBtn = $("<a href=" + queryLink + " class='link'>");
 
     var image = $('<img src="assets/images/sports-block.jpg" alt="sportsBlock" class="sports-img">');
-    var label = $("<div class='label'>"+ sports[i]+"</div>");
+    var label = $("<div class='label'>" + sports[i] + "</div>");
     var innerBlock = sportsBtn.append(image).append(label);
 
     var token = imgDiv.append(innerBlock);
@@ -39,20 +63,19 @@ function createButtons() {
   }
 }
 
-  function sportsInfo(){
+function sportsInfo() {
 
-      //Saves search term in variable for queries
-      let sportItem = $(this).attr("data-name");
-      
-      //Redirects user to info.html page
-      window.location.replace("./info.html");
+  //Saves search term in variable for queries
+  let sportItem = $(this).attr("data-name");
 
-      //TODO: ajax requests will go here
+  //Redirects user to info.html page
+  window.location.replace("./info.html");
+
+  //TODO: ajax requests will go here
 
 
-      
-  }
 
-  $(document).on("click", ".sports-btn", sportsInfo)
+}
 
+$(document).on("click", ".sports-btn", sportsInfo)
 
